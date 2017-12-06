3b9b6f9a592e398129939e761404676e9658baf3
alainakwan <kwan.alaina@gmail.com>
2017-12-05 16:15:05 -0800

    started sorting the data

--- data/comicvine-data.R
+++ data/comicvine-data.R
@@ -1,7 +1,28 @@
 # File to combine .csv files 
+library(dplyr)
 
-comicvine.data1 <- read.csv('comicvine_data1.csv', stringsAsFactors = FALSE)
-comicvine.data2 <- read.csv('comicvine_data2.csv', stringsAsFactors = FALSE)
-comicvine.data3 <- read.csv('comicvine_data3.csv', stringsAsFactors = FALSE)
+comicvine.data1 <- read.csv('data/comicvine_data1.csv', stringsAsFactors = FALSE, fileEncoding = 'latin1')
+comicvine.data2 <- read.csv('data/comicvine_data2.csv', stringsAsFactors = FALSE, fileEncoding = 'latin1')
+comicvine.data3 <- read.csv('data/comicvine_data3.csv', stringsAsFactors = FALSE, fileEncoding = 'latin1')
 
 comicvine.data <- rbind(comicvine.data1, comicvine.data2, comicvine.data3)
+
+dc.male.characters <- comicvine.data %>%
+  select(name, real_name, gender, image.small_url, publisher.name) %>%
+  filter(gender == '1') %>%
+  filter(publisher.name == 'DC Comics')
+
+dc.female.characters <- comicvine.data %>%
+  select(name, real_name, gender, image.small_url, publisher.name) %>%
+  filter(gender == '2') %>%
+  filter(publisher.name == 'DC Comics')
+
+marvel.male.characters <- comicvine.data %>%
+  select(name, real_name, gender, image.small_url, publisher.name) %>%
+  filter(gender == '1') %>%
+  filter(publisher.name == 'Marvel')
+
+marvel.female.characters <- comicvine.data %>%
+  select(name, real_name, gender, image.small_url, publisher.name) %>%
+  filter(gender == '2') %>%
+  filter(publisher.name == 'Marvel')
---
+++ server.R
@@ -0,0 +1,7 @@
+# Server file for final project
+
+library(shiny)
+library(dplyr)
+
+# Read in data
+source('data/comicvine-data.R')
