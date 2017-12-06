59f192b9ecce67f745ae50801a73a35bf255a8c3
Michelle Gouw <m.gouw999@gmail.com>
2017-12-06 00:55:50 -0800

    Add sfunction to generate barchart

---
+++ scripts/GenerateBarchart.R
@@ -0,0 +1,45 @@
+# Load Libraries 
+library(dplyr)
+library(plotly)
+
+# Import comicvine data 
+#source('./data/comicvine-data.R')
+source('./data/random.R')
+
+gender.counts <- comicvine.data %>%
+  group_by(publisher.name) %>%
+  count(gender)
+
+# Function to generate barchart of gender diversity by publisher
+generateBarchart <- function(data, publisher1 = 'DC Comics', publisher2 = 'Marvel') {
+
+  desired.publishers <- gender.counts %>%
+    filter((publisher.name == publisher1 | publisher.name == publisher2) &
+             (gender == 1 | gender == 2))
+  
+  without.publisher <- desired.publishers %>%
+    ungroup() %>%
+    select(gender, n)
+  
+  males <- without.publisher %>%
+    filter(gender == 1) %>%
+    select(n)
+  
+  females <- without.publisher %>%
+    filter(gender == 2) %>%
+    select(n)
+  
+  publisher.name <- c(unique(desired.publishers$publisher.name))
+  desired.df <- data.frame(publisher.name, males, females)
+  colnames(desired.df)[2:3] <- c("males", "females")
+  
+  p <- plot_ly(desired.df, x = ~publisher.name, y = ~males, type = 'bar', name = "Males",
+               hovertext = ~paste0("State: ", "<br>$", "M")) %>%
+    add_trace(y = ~females, name = "Females") %>%
+    layout(title = paste("Gender of Comic Book Characters by Publisher"),
+           xaxis = list(title = "Publisher"),
+           yaxis = list(title = "Count")
+    )
+  return(p)
+}
+
