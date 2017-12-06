16bb3b2a69f5282a373bb5deb0cb9cf23f95ef8b
Michelle Gouw <m.gouw999@gmail.com>
2017-12-05 17:06:39 -0800

    Make scripts folder

--- data/comicvine-data.R
+++ data/comicvine-data.R
@@ -1,7 +1,7 @@
 # File to combine .csv files 
 
-comicvine.data1 <- read.csv('comicvine_data1.csv', stringsAsFactors = FALSE)
-comicvine.data2 <- read.csv('comicvine_data2.csv', stringsAsFactors = FALSE)
-comicvine.data3 <- read.csv('comicvine_data3.csv', stringsAsFactors = FALSE)
+comicvine.data1 <- read.csv('data/comicvine_data1.csv', stringsAsFactors = FALSE)
+comicvine.data2 <- read.csv('data/comicvine_data2.csv', stringsAsFactors = FALSE)
+comicvine.data3 <- read.csv('data/comicvine_data3.csv', stringsAsFactors = FALSE)
 
 comicvine.data <- rbind(comicvine.data1, comicvine.data2, comicvine.data3)
