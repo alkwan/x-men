e42b70f8121a013d134a065aac83eebba63a3181
alainakwan <kwan.alaina@gmail.com>
2017-12-05 18:01:22 -0800

    got rid of individual publisher data frames and edited server stuff

--- data/comicvine-data.R
+++ data/comicvine-data.R
@@ -1,41 +1,25 @@
 # File to combine .csv files 
 library(dplyr)
 
+# Read in CSVs
 comicvine.data1 <- read.csv('data/comicvine_data1.csv', stringsAsFactors = FALSE, fileEncoding = 'latin1')
 comicvine.data2 <- read.csv('data/comicvine_data2.csv', stringsAsFactors = FALSE, fileEncoding = 'latin1')
 comicvine.data3 <- read.csv('data/comicvine_data3.csv', stringsAsFactors = FALSE, fileEncoding = 'latin1')
 
-
+# Combine CSVs
 comicvine.data <- rbind(comicvine.data1, comicvine.data2, comicvine.data3)
 
+# Get all available publisher names
 publishers <- comicvine.data %>%
   select(publisher.name) %>%
   unique()
 
+# Get all male characters
 male.characters <- comicvine.data %>%
   select(name, real_name, gender, image.small_url, publisher.name) %>%
   filter(gender == '1')
 
+# Get all female characters
 female.characters <- comicvine.data %>%
   select(name, real_name, gender, image.small_url, publisher.name) %>%
   filter(gender == '2')
-
-dc.male.characters <- male.characters %>%
-  filter(publisher.name == 'DC Comics')
-
-dc.female.characters <- female.characters %>%
-  filter(publisher.name == 'DC Comics')
-
-marvel.male.characters <- male.characters %>%
-  filter(publisher.name == 'Marvel')
-
-marvel.female.characters <- comicvine.data %>%
-  select(name, real_name, gender, image.small_url, publisher.name) %>%
-  filter(gender == '2') %>%
-  filter(publisher.name == 'Marvel')
-
-image.male.characters <- male.characters %>%
-  filter(publisher.name == 'Image')
-
-image.female.characters <- female.characters %>%
-  filter(publisher.name == 'Image')
--- server.R
+++ server.R
@@ -3,8 +3,26 @@
 library(shiny)
 library(dplyr)
 
-# Read in data
-source('data/comicvine-data.R')
+# Set working directory
+# setwd('~/documents/documents/uw_homework/info_201/assignments/x-men')
 
-male.characters <- comicvine.data %>%
-  select(name, gender, image.small_url, publisher.name)
+# Read in the data
+source('./data/comicvine-data.R')
+
+shinyServer(function(input, output) {
+  
+  # Make the barchart of characters by gender per publisher
+  output$chart <- renderPlot({
+    # Grab all the male characters from the inputted publisher
+    publisher.male.characters <- male.characters %>%
+      filter(publisher.name == input$publisher)
+    
+    # Grab all the female characters from the inputted publisher
+    publisher.female.characters <- female.characters %>%
+      filter(publisher.name == input$publisher)
+    
+    # Render the bar chart
+    
+  })
+})
+
