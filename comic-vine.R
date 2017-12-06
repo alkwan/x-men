ddc5994fb473809e0649ad0dbbf13f623637a297
Omar Ahmer Azeemi <oazeemi@gmail.com>
2017-12-06 13:55:22 -0800

    Added table information the the server file, and added functions to the comic-vine file

--- comic-vine.R
+++ comic-vine.R
@@ -3,60 +3,70 @@ library(dplyr)
 library(httr)
 
 source('comic-key.R')
-source('ui.R')
 
 url <- "http://comicvine.com/api/"
 api <- "/?api_key="
 
-# Gender data
-gender.url <- paste0(url, 'characters', api, comic.vine, '&filter=gender:female&field_list=name,id,birth,publisher,first_appeared_in_issue,image,count_of_issue_appearances&format=json')
-gender.response <- GET(gender.url)
-gender.body <- content(gender.response, "text")
-gender.results <- fromJSON(gender.body) 
-data.gender <- flatten(gender.results$results)
+# # Gender data
+# gender.url <- paste0(url, 'characters', api, comic.vine, '&filter=gender:female&field_list=name,id,birth,publisher,first_appeared_in_issue,image,count_of_issue_appearances&format=json')
+# gender.response <- GET(gender.url)
+# gender.body <- content(gender.response, "text")
+# gender.results <- fromJSON(gender.body) 
+# data.gender <- flatten(gender.results$results)
 
 # Character chosen by user
-name <- "wonder woman"
-chosen.url <- paste0(url, 'characters', api, comic.vine, '&filter=name:', name, '&field_list=name,gender,id,birth,publisher,first_appeared_in_issue,count_of_issue_appearances,image&sort=count_of_issue_appearances:desc&format=json')
-chosen.response <- GET(chosen.url)
-chosen.body <- content(chosen.response, "text")
-chosen.results <- fromJSON(chosen.body) 
-data.chosen <- flatten(chosen.results$results)
+CharacterAllInfo <- function(name) {
+  chosen.url <- paste0(url, 'characters', api, comic.vine, '&filter=name:', name, '&field_list=name,gender,id,birth,publisher,first_appeared_in_issue,count_of_issue_appearances,image&sort=count_of_issue_appearances:desc&format=json')
+  chosen.response <- GET(chosen.url)
+  chosen.body <- content(chosen.response, "text")
+  chosen.results <- fromJSON(chosen.body) 
+  data.chosen <- flatten(chosen.results$results)
+  return(data.chosen)
+}
 
 # Information about the character chosen
-character.id <- data.chosen$id
-character.id <- character.id[1]
+CharacterChosenInfo <- function(data.chosen) {
+  character.id <- data.chosen$id
+  character.id <- character.id[1]
+  character <- paste0(url, 'character/4005-', character.id, api, comic.vine, '&offset=5&format=json')
+  response.character <- GET(character)
+  body.character <- content(response.character, "text")
+  results.character <- fromJSON(body.character)
+  convert.all <- data.frame(t(sapply(results.character$results,c)))
+  return(convert.all)
+}
 
-character <- paste0(url, 'character/4005-', character.id, api, comic.vine, '&format=json')
-response.character <- GET(character)
-body.character <- content(response.character, "text")
-results.character <- fromJSON(body.character)
-convert.all <- data.frame(t(sapply(results.character$results,c)))
 
-# Get friends
-convert.friends <- data.frame(t(sapply(convert.all$character_friends,c)))
-get.friends <- data.frame(t(sapply(convert.friends$name,c)))
+# # Get friends
+# convert.friends <- data.frame(t(sapply(convert.all$character_friends,c)))
+# get.friends <- data.frame(t(sapply(convert.friends$name,c)))
 
 # Get powers
-convert.powers <- data.frame(t(sapply(convert.all$powers,c)))
-get.powers <- data.frame(t(sapply(convert.powers$name,c)))
+GetPowers <- function(convert.all) {
+  convert.powers <- data.frame(t(sapply(convert.all$powers,c)))
+  powers <- data.frame(t(sapply(convert.powers$name,c)))
+  return(powers)
+}
 
-# Issue
-issue <- data.chosen$first_appeared_in_issue.id
-issue <- issue[1]
-issue.url <- paste0(url, 'issue/4000-', issue, api, comic.vine, '&format=json')
-response.issue <- GET(issue.url)
-body.issue <- content(response.issue, "text")
-results.issue <- fromJSON(body.issue)
 
-data.issue <- data.frame(t(sapply(results.issue$results,c)))
-issue.date <- data.frame(t(sapply(data.issue$cover_date,c))) # Gets the date when the character was first introduced
-
-# Types
-type <- paste0(url, 'types', api, comic.vine, '&format=json')
-response.type <- GET(type)
-body.type <- content(response.type, "text")
-results.type <- fromJSON(body.type)
-data.type <- flatten(results.type$results)
-
+# Get the date of when the character was first published
 # Assumption made is that the character with the most appearances is the original character
+GetDate <- function(data.chosen) {
+  issue <- data.chosen$first_appeared_in_issue.id
+  issue <- issue[1]
+  issue.url <- paste0(url, 'issue/4000-', issue, api, comic.vine, '&format=json')
+  response.issue <- GET(issue.url)
+  body.issue <- content(response.issue, "text")
+  results.issue <- fromJSON(body.issue)
+  data.issue <- data.frame(t(sapply(results.issue$results,c)))
+  issue.date <- data.frame(t(sapply(data.issue$cover_date,c)))
+  return(issue.date)
+}
+
+# # Types
+# type <- paste0(url, 'types', api, comic.vine, '&format=json')
+# response.type <- GET(type)
+# body.type <- content(response.type, "text")
+# results.type <- fromJSON(body.type)
+# data.type <- flatten(results.type$results)
+# 
--- server.R
+++ server.R
@@ -8,6 +8,7 @@ library(dplyr)
 
 # Read in the data
 source('./data/comicvine-data.R')
+source('comic-vine.R')
 
 shinyServer(function(input, output) {
   
@@ -27,12 +28,38 @@ shinyServer(function(input, output) {
   
   output$character.comparison <- renderTable({
     # Get the female character data
-    female.character <- female.characters %>%
-      filter(name == input$female)
+    # female.character <- female.characters %>%
+    #   filter(name == input$female)
+    data.chosen.female <- CharacterAllInfo('Superman')
+    convert.all.female <- tryCatch(
+      CharacterChosenInfo(data.chosen.female),
+      error = function(error_message) {
+        powers.female <- "Not available"
+      })
+    if(convert.all.female[1] == "Not available") {
+      powers.female <- "Not available"
+    } else {
+      powers.female <- GetPowers(convert.all.female)
+    }
+    date.female <- GetDate(data.chosen.female)
+    appearances.female <- data.chosen.female$count_of_issue_appearances
     
     # Get the male character data
-    male.character <- male.characters %>%
-      filter(name == input$male)
+    # male.character <- male.characters %>%
+    #   filter(name == input$male)
+    data.chosen.male <- CharacterAllInfo(input$male)
+    convert.all.male <- tryCatch(
+      CharacterChosenInfo(data.chosen.male),
+      error = function(error_message) {
+        powers.male <- "Not available"
+      })
+    if(convert.all.male[1] == "Not available") {
+      powers.male <- "Not available"
+    } else {
+      powers.male <- GetPowers(convert.all.male)
+    }
+    date.male <- GetDate(data.chosen.male)
+    appearances.male <- data.chosen.male$count_of_issue_appearances
     
     # Make the comparison table
   })
