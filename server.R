b23835748d98d30e69adcfbac8719f433a51d8fd
Omar Ahmer Azeemi <oazeemi@gmail.com>
2017-12-07 12:48:15 -0800

    Added table and changes

--- server.R
+++ server.R
@@ -18,49 +18,19 @@ shinyServer(function(input, output) {
   # Picture output for character comparison
   female.chosen <- reactive({female.characters %>% filter(name == input$female)})
   male.chosen <- reactive({male.characters %>% filter(name == input$male)})
+  male.name <- reactive({male.names %>% filter(name == input$male)})
   src.1 <- reactive({female.chosen()$image.screen_url})
   src.2 <- reactive({male.chosen()$image.screen_url})
   output$picture.1<-renderText({c('<img src="',src.1()[1],'">')})
   output$picture.2<-renderText({c('<img src="',src.2()[1],'">')})
+  comparison.frame <- reactive({data.frame(male.names()$name)})
   
   # The table of character comparison data.
   output$character.comparison <- renderTable({
-    # Get the female character data
-    # female.character <- female.characters %>%
-    #   filter(name == input$female)
-    data.chosen.female <- CharacterAllInfo('Superman')
-    convert.all.female <- tryCatch(
-      CharacterChosenInfo(data.chosen.female),
-      error = function(error_message) {
-        powers.female <- "Not available"
-      })
-    if(convert.all.female[1] == "Not available") {
-      powers.female <- "Not available"
-    } else {
-      powers.female <- GetPowers(convert.all.female)
-    }
-    date.female <- GetDate(data.chosen.female)
-    appearances.female <- data.chosen.female$count_of_issue_appearances
-    
-    # Get the male character data
-    # male.character <- male.characters %>%
-    #   filter(name == input$male)
-    data.chosen.male <- CharacterAllInfo(input$male)
-    convert.all.male <- tryCatch(
-      CharacterChosenInfo(data.chosen.male),
-      error = function(error_message) {
-        powers.male <- "Not available"
-      })
-    if(convert.all.male[1] == "Not available") {
-      powers.male <- "Not available"
-    } else {
-      powers.male <- GetPowers(convert.all.male)
-    }
-    date.male <- GetDate(data.chosen.male)
-    appearances.male <- data.chosen.male$count_of_issue_appearances
-    
-    # Make the comparison table
-  })
+    head(comparison.frame(), n = 6)},
+    bordered = TRUE,
+    hover = TRUE, spacing = 'l',
+    align = 'c')
   
   # Output either the bar chart or the pie chart.
   output$type_of_chart <- renderPlotly({ 
