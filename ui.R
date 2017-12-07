27eeb117d880ab88230b3eceadadb3d6a75e2c1f
alainakwan <kwan.alaina@gmail.com>
2017-12-06 18:25:21 -0800

    edited descriptions and adjusted UI

--- ui.R
+++ ui.R
@@ -42,25 +42,15 @@ shinyUI(
         ),
                 
       mainPanel(
-        h4("Shot Chart of Players"),
-        splitLayout(cellwidths=c("50%","50%"),htmlOutput("picture.1"),htmlOutput("picture.2")),
-        h4("Comparison of shot attempts"),
-        splitLayout(cellwidths=c("50%","50%"),plotlyOutput("plot11"),plotlyOutput("plot12")),
-        h4("Comparison of % Field Goal"),
-        splitLayout(cellwidths=c("50%","50%"),plotlyOutput("plot13"),plotlyOutput("plot14")),
-        h4("Comparison of shooting preference by Distance"),
-        plotlyOutput("plot15"),
-        h4("Comparison of Offense Diversity (Bar)"),
-        splitLayout(cellwidths=c("50%","50%"),plotlyOutput("plot16"),plotlyOutput("plot17")),
-        h4("Comparison of Offense Diversity (Pie)"),
-        splitLayout(cellwidths=c("50%","50%"),plotlyOutput("plot18"),plotlyOutput("plot19"))
-        ))
-      ),
-        
+        h4("Character Comparison"),
+        splitLayout(cellwidths=c("50%","50%"),htmlOutput("picture.1"),htmlOutput("picture.2"))
+      )
+    )
+  ),   
           
     tabPanel("Report by Publisher", sidebarLayout(
       sidebarPanel(
-        h4("need a title"),
+        h4("Gender by Publisher"),
         selectizeInput("publisher1", "Publisher 1", publishers, multiple = FALSE,
                        options = list(maxOptions = 5, placeholder = 'Please type in the name', 
                                       onInitialize = I('function() { this.setValue("DC Comics"); }'))),
@@ -71,11 +61,11 @@ shinyUI(
       
       mainPanel(
         h4("Description"),
-        p(h5("This special part serves the research function of re-clustering the habit of shot of players. Traditionally, the league always cluster the shot of players by the defined area of the court. However, the revolution of the training technique makes some players really crazy. Therefore, it is not quite scientific to research the shot data by the traditional court division. Therefore, we implement K-means cluster analysis to re-group the shot data of each player. We believe that clustering the shot data for each player will be beneficial to formulate the defending strategy against the player. For a commonly asked question referring to the cluster analysis, the number of clusters, we think the # of clusters is better decided by the user him/herself. The scientific number of clusters can be visualized by the specific shooting plot",align="Justify")),
-        h4("Clustering analysis for all shot data"),
-        p(h5("Firstly, we group all data of the player. Different colors represent different clusters")),
+        p("Type in two publisher names and see the number breakdown of their female and male characters.
+          Switch between bar charts and pie charts to see numbers versus percentages."),
         plotlyOutput('chart'),
         plotlyOutput('pie')
+      )
     )
   ),
                    
@@ -90,27 +80,26 @@ shinyUI(
                                        div.checkbox {margin-top: -10px;
                                        margin-bottom: -25px;}"))),
           
-             sidebarLayout(
-      sidebarPanel(
-        controls <-list(tags$div(align = 'left', 
-                                 class = 'multicol', 
-                                 radioButtons(inputId  = 'year', 
-                                              label = "",
-                                              choices  = NULL,
-                                              selected = NULL,
-                                              inline   = FALSE, 
-                                              choiceNames = c("2000", "2001", "2002", "2003", "2004", "2005"), 
-                                              choiceValues = c(2000, 2001, 2002, 2003, 2004, 2005)) 
-                                 )),
-        
-        h4("need a title"),
-        radioButtons("test", "Select a year", choices = NULL, selected = NULL,
-                     inline = FALSE, width = NULL, choiceNames = c("2000", "2001"), choiceValues = c(2000, 2001))
-      ),
+      sidebarLayout(
+        sidebarPanel(
+          controls <-list(tags$div(align = 'left', 
+                                   class = 'multicol', 
+                                   radioButtons(inputId  = 'year', 
+                                                label = "",
+                                                choices  = NULL,
+                                                selected = NULL,
+                                                inline   = FALSE, 
+                                                choiceNames = c("2000", "2001", "2002", "2003", "2004", "2005"), 
+                                                choiceValues = c(2000, 2001, 2002, 2003, 2004, 2005)
+                                   ))),
+          
+          h4("need a title"),
+          radioButtons("test", "Select a year", choices = NULL, selected = NULL,
+                       inline = FALSE, width = NULL, choiceNames = c("2000", "2001"), choiceValues = c(2000, 2001))
+        ),
       
-      mainPanel(dataTableOutput("shotlog"))
+        mainPanel(dataTableOutput("shotlog"))
+      )
     )
-    )
-  )
   )
 )
